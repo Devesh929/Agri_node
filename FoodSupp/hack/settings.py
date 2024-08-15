f99882221be0058496cf9f60b6c056f11f24fd3b
dd <46255390+Devesh929@users.noreply.github.com>
2024-08-16 00:46:48 +0530

    Update settings.py

--- FoodSupp/hack/settings.py
+++ FoodSupp/hack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9($sq=i*bo$^!s332&18dn5lyvs-9wgjjm^j*)wu1#pn6a(nyt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
